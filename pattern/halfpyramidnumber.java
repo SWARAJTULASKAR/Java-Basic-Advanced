ed60b503bf74202e99dd7ffa298267a822c47a67
SWARAJTULASKAR <swarajtulaskar2003@gmail.com>
2025-02-24 01:32:45 +0530

    New Basic Pattern Program and their output ...

---
+++ pattern/halfpyramidnumber.java
@@ -0,0 +1,18 @@
+
+public class halfpyramidnumber {
+
+    public static void main(String args[]) {
+        int n = 5;
+        int m = 5;
+        //  outer loop
+        for (int i = 1; i <= n; i++) {
+            //inner  loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+}
